c42f66683fb82f99210c9d450ef3cc5d6703e824
DogRotation <110861910+DogRotation@users.noreply.github.com>
2022-08-31 16:11:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,63 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+let engine;
+let world;
+
+var ground;
+var wall1, wall2;
+var ball;
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var ballOptions = {
+		isStatic: false,
+		restitution: 0.3,
+		friction: 0.3,
+		density: 1.2
+	}
+
+	//Create the Bodies Here.
+	ground = new Ground(400,480,800,30);
+	wall1 = new Ground(460,450,15,30);
+	wall2 = new Ground(360,450,15,30);
+	ball =  Bodies.circle(150,452,12, ballOptions); 	
+
+	
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+	ground.show();
+	wall1.show();
+	wall2.show();
+	ellipse(ball.position.x,ball.position.y,12);
+	Engine.run(engine);
+	
+}
+
+function keyPressed(){
+	if(keyCode === RIGHT_ARROW){
+		Matter.Body.applyForce(ball,{x: 0,y: 0}, {x: 8,y: 0});
+	}
+	if(keyCode === LEFT_ARROW){
+		Matter.Body.applyForce(ball,{x: 0,y:0}, {x: 0, y: -8});
+	}
+}
+
+
+
+
